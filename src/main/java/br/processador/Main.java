7375b2a9b536117e96ef9833a70c7fc96c45ec4f
Paulo Sergio <036paulo@gmail.com>
2025-07-23 16:32:19 -0300

    Ajustes para melhorar a experiência do usuário e a agilidade

--- src/main/java/br/processador/Main.java
+++ src/main/java/br/processador/Main.java
@@ -10,34 +10,77 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    final String VERSAO = "1.0.0";
+    final String VERSAO = "1.0.1";
 
-    System.out.print("""
-            **************************************
-            *         VERSÃO %s               *
-            **************************************
-           
-            """.formatted(VERSAO));
+    System.out.printf("""
+    **************************************
+    *         VERSÃO %s               *
+    **************************************
+   
+    """, VERSAO );
     try (Scanner scanner = new Scanner(System.in)) {
-      System.out.print("""
-              SUPORTE (mysql / postgresql / sqlserver)
-              Informe o tipo do banco:""");
-      Dialect dialect = Dialect.fromString(scanner.nextLine().trim());
 
-      if (dialect == null) {
-        System.err.println("**** Banco de dados não suportado. ****");
-        return;
+      System.out.print("""
+              1: mysql
+              2: postgresql
+              3: sqlserver
+              Informe o tipo do banco: """);
+      String bancoInput = scanner.nextLine().trim();
+      String banco;
+      switch (bancoInput){
+        case "1" -> banco="mysql";
+        case "2" -> banco="postgresql";
+        case "3" -> banco="sqlserver";
+
+        default -> {
+          System.err.println("**** Banco de dados não suportado. ****");
+          return;
+        }
       }
 
+      Dialect dialect = Dialect.fromString(banco);
+
       System.out.print("""
-              SUPORTE: (localhost , 127.0.0.1)
+              1: localhost
+              2: 127.0.0.1
+              3: outros
               Informe o host:""");
       String host = scanner.nextLine().trim();
 
+      switch (host){
+        case "1" -> host = "localhost";
+        case "2" -> host = "127.0.0.1";
+        case "3" -> {
+          System.out.println("Informe o host: ");
+          host = scanner.nextLine().trim();
+        }
+        default -> {
+          System.err.println("**** Opção invalida ***");
+          return;
+        }
+      }
+
       System.out.print("""
-              SUPORTE: (PG: 5432, SQL SERVER: 1433, MYSQL: 3306)
+              1: 3306
+              2: 5432
+              3: 1433
+              4: OUTROS
               Informe a porta:""");
       String port = scanner.nextLine().trim();
+
+      switch (port){
+        case "1" -> port = "3306";
+        case "2" -> port = "5432";
+        case "3" -> port = "1433";
+        case "4" -> {
+          System.out.println("Informe o host: ");
+          port = scanner.nextLine().trim();
+        }
+        default -> {
+          System.err.println("**** Opção invalida ***");
+          return;
+        }
+      }
 
       // caminho C:/MeusBancos/bairros.fdb
       System.out.print("""
@@ -46,20 +89,40 @@ public class Main {
       String database = scanner.nextLine().trim();
 
       System.out.print("""
-              SUPORTE: ( PG: postgres, SQL SERVER: sa, MYSQL: root )
-              Informe o usuário:""");
+              1: root
+              2: postgres
+              3: sa
+              4: outros
+              Informe o usuário: """);
       String user = scanner.nextLine().trim();
+
+      switch (user){
+        case "1" -> user = "root";
+        case "2" -> user = "postgres";
+        case "3" -> user = "sa";
+        case "4" -> {
+          System.out.println("Informe o host: ");
+          user = scanner.nextLine().trim();
+        }
+        default -> {
+          System.err.println("**** Opção invalida ***");
+          return;
+        }
+      }
 
       System.out.println("""
               SUPORTE: ( CADA BANCO TEM SUA PARTICULARIDADE )
-              Informe a senha:""");
+              Informe a senha: """);
       String password = scanner.nextLine().trim();
+
       System.out.print("""
               SUPORTE: ( CAMINHO DA PASTA COMPLETO ) *** ARQUIVO DEVE SER ( ARQUIVO.sql) ***
               Informe o caminho da pasta com arquivos .sql:""");
       String path = scanner.nextLine().trim();
 
-      try (Connection connection = ConnectionFactory.getConnection(dialect, host, port, database, user, password)) {
+        assert dialect != null;
+
+        try (Connection connection = ConnectionFactory.getConnection(dialect, host, port, database, user, password)) {
         ConsultaExecutor executor = new ConsultaExecutor(connection);
         executor.executarConsultasEmPasta(new File(path));
       }
