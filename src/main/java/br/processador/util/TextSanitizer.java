c6a0c724e5f6b24b67be55cdfedca66002238b91
Paulo Sergio <036paulo@gmail.com>
2025-08-11 18:34:49 -0300

    feat(TextSenitizer) ajustado para alterar os numeros e remover o ponto e virgula

--- src/main/java/br/processador/util/TextSanitizer.java
+++ src/main/java/br/processador/util/TextSanitizer.java
@@ -4,12 +4,16 @@ import java.text.Normalizer;
 
 public class TextSanitizer {
   public static String sanitize(String input) {
-    if (input == null) return "";
+    if (input == null || input.trim().isEmpty()) return "";
     return Normalizer.normalize(input, Normalizer.Form.NFD)
             .replaceAll("[\\p{InCombiningDiacriticalMarks}]", "")
             .replaceAll("[\\t\\n\\r]+", " ")
             .replaceAll(" +", " ")
-            .replaceAll("[ªº°]","")
+            .replaceAll("[ªº°'\"]","")
+            .replaceAll("¹","1")
+            .replaceAll("²","2")
+            .replaceAll("³","3")
+            .replaceAll(";",":")
             .trim();
   }
 }
